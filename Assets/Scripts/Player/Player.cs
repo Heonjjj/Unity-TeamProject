55e5909bdb0c1ad571f762c17bd4e5f03322cfd2
SJW1737 <sic470212@naver.com>
2025-07-30 14:41:38 +0900

    [feat] 카메라 팔로우 생성, 캐릭터 사거리 생성

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,10 +6,11 @@ public class Player : Character
 {
     protected override void Start()
     {
-        maxHP = 100f;
+        maxHP = 5f;
         attackPower = 10f;
         attackSpeed = 1f;
-        moveSpeed = 6f;                             //스탯 설정 수정 가능
+        moveSpeed = 6f;
+        attackRange = 5f;                           //스탯 설정 수정 가능
 
         base.Start();                               //최대 체력으로 맞춰주기         
     }
