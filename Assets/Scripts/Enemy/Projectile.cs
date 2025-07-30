a05cf46f2b8687142b87896b40ea6c9f746d5ed3
kiub-15 <kmk923140@gmail.com>
2025-07-30 20:50:11 +0900

    [fix] 발사체가 안없어지는 버그 수정

--- Assets/Scripts/Enemy/Projectile.cs
+++ Assets/Scripts/Enemy/Projectile.cs
@@ -2,20 +2,32 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    public int damage = 1;
-    public float lifeTime = 5f;
+    [SerializeField] private float lifeTime = 5f;
+    private bool isDestroyed = false; // 중복 제거 오류 방지
 
-    private void Start()
+    private void OnEnable()
     {
-        Destroy(gameObject, lifeTime);
+        StartCoroutine(DestroyAfterTime());
+    }
+
+    private System.Collections.IEnumerator DestroyAfterTime()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        DestroyProjectile();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") || collision.CompareTag("Wall"))
         {
-            Debug.Log("원거리 공격 피격!");
-            Destroy(gameObject);
+            DestroyProjectile();
         }
+    }
+
+    private void DestroyProjectile()
+    {
+        if (isDestroyed == true) return; // 이미 제거된 경우 다시 실행하지 않음
+        isDestroyed = true;
+        Destroy(gameObject);
     }
 }
