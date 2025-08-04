d500d5e9d489c60029d966062784f559f8362e51
kyubeom1 <a64778371@gmail.com>
2025-08-04 11:46:43 +0900

    [feat]멀티샷 스킬 보유시 스킬 선택 UI에서 삭제

--- Assets/Scripts/UpgradeManager.cs
+++ Assets/Scripts/UpgradeManager.cs
@@ -23,8 +23,8 @@ public class UpgradeManager : MonoBehaviour
         skillList.Add(new Skill("데미지증가", "데미지가 2 증가합니다", 2, value: 2));
         skillList.Add(new Skill("사거리증가", "사거리가 1 증가합니다", 3, value: 1));
         skillList.Add(new Skill("이동속도증가", "이동속도가 1 증가합니다", 4, value: 1));
-        skillList.Add(new Skill("더블샷","화살을 두 발씩 발사합니다", 5, value:0));
-        skillList.Add(new Skill("트리플샷", "화살을 세 발씩 발사합니다", 6, value:0));
+        skillList.Add(new Skill("트리플샷", "화살을 세 발씩 발사합니다", 5, value: 0));
+        skillList.Add(new Skill("더블샷", "화살을 두 발씩 발사합니다", 6, value: 0));
     }
     public void Start()
     {
@@ -56,11 +56,11 @@ public class UpgradeManager : MonoBehaviour
                 break;
 
             case 5:
-                player.GetComponent<PlayerAttack>().multiShot2 = true;
+                player.GetComponent<PlayerAttack>().multiShot3 = true;                
                 break;
 
             case 6:
-                player.GetComponent<PlayerAttack>().multiShot3 = true;
+                player.GetComponent<PlayerAttack>().multiShot2 = true;
                 break;
         }
     }
--- Assets/Scripts/UpgradeUI.cs
+++ Assets/Scripts/UpgradeUI.cs
@@ -8,6 +8,14 @@ public class UpgradeUI : MonoBehaviour
 {
     int selectSkill = 3;
     public List<UpgradeBtn> upgradeBtn;
+    Player player;
+    public void Awake()
+    {
+        if (player == null)
+        {
+            player = FindObjectOfType<Player>();
+        }
+    }
     public void Start()
     {
         UpgradeImage();
@@ -16,7 +24,19 @@ public class UpgradeUI : MonoBehaviour
     {        
         gameObject.SetActive(true);
         List<Skill> tempList = new List<Skill>(UpgradeManager.Instance.skillList);
-        //와일문 한 번 써보기
+        if (player.GetComponent<PlayerAttack>().multiShot2)//시간 관계상 내가 생각한 방법으로 진행
+        {
+            tempList.Remove(tempList[6]);
+        }
+        else
+        {
+            tempList.Remove(tempList[5]);
+        }
+        if (player.GetComponent<PlayerAttack>().multiShot3)
+        {
+            tempList.Remove(tempList[5]);
+        }
+        //플레이어에서 체크 하고 체크가 돼있으면 해당하는 스킬을 제거
         for (int i = 0; i < selectSkill; i++)
         {
             Skill randomSkill = tempList[Random.Range(0, tempList.Count)];//랜덤 스킬 함수에 저장된 랜덤번째의 스킬을 randomSkill변수에 저장
@@ -26,13 +46,13 @@ public class UpgradeUI : MonoBehaviour
             //Skill randomSkill = RandomSkill();
             //if (i == 0)
             //{
-            //    upgradeBtn[0].SetSkill(randomSkill);//i
+            //    upgradeBtn[i].SetSkill(randomSkill);//i
             //}
             //else 
             //{
-            //    if (upgradeBtn[0] != null)//i==1
+            //    if (i==1)
             //    {
-            //        while (randomSkill != upgradeBtn[0].GetSkill())//i-1 랜덤스킬이랑 업그레이드버튼 0번의 스킬이 다를 때 랜덤 스킬을 다시 뽑아야 함
+            //        while (upgradeBtn[i-1]= upgradeBtn[0].GetSkill())//i-1 랜덤스킬이랑 업그레이드버튼 0번의 스킬이 다를 때 랜덤 스킬을 다시 뽑아야 함
             //        {
             //            upgradeBtn[1].SetSkill(randomSkill);//randomSkill=RandomSkill();
             //        }
@@ -51,10 +71,10 @@ public class UpgradeUI : MonoBehaviour
             //    while (업그레이드 버튼[i - 1].skill == randomskill)                
         }        
     }
-    Skill RandomSkill()
-    {
-        Skill r = UpgradeManager.Instance.skillList[Random.Range(0, UpgradeManager.Instance.skillList.Count)];
-        //업그레이드 매니저에 싱글톤화된 스킬리스트의 0번째 부터 스킬리스트의 마지막번째 순서까지의 랜덤번째 순서를 r에 저장
-        return r;//랜덤 저장된 스킬 r을 리턴을 통해 랜덤스킬 함수에 다시 스킬 형태로 줌
-    }
+    //Skill RandomSkill()
+    //{
+    //    Skill r = UpgradeManager.Instance.skillList[Random.Range(0, UpgradeManager.Instance.skillList.Count)];
+    //    //업그레이드 매니저에 싱글톤화된 스킬리스트의 0번째 부터 스킬리스트의 마지막번째 순서까지의 랜덤번째 순서를 r에 저장
+    //    return r;//랜덤 저장된 스킬 r을 리턴을 통해 랜덤스킬 함수에 다시 스킬 형태로 줌
+    //}
 }
